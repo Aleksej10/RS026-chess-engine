ddf9701efa1c35fb10362d6842fe0972dd578ba7
wnsmith <winstonsmithnolan@gmail.com>
2019-01-12 20:29:31 +0100

    separating files to directories

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "headers/mainwindow.h"
+#include <QApplication>
+#include "QStyleFactory"
+#include "QDebug"
+#include "QDir"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+
+    QPixmap bkgnd(":/images/images/background.png");
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bkgnd);
+    w.setPalette(palette);
+
+    w.show();
+
+    return a.exec();
+}
